5bfe4deea4b66f8cf34d2fa9f040336953228c3a
Nick Zart <nzart@g.clemson.edu>
2019-07-11 17:00:36 -0400

    UPDATE: 07.11.19
    New styles

---
+++ script.js
@@ -0,0 +1,26 @@
+var nav = document.querySelector('.nav');
+var logo = document.querySelector('.logo');
+var first = document.getElementById('first');
+var second = document.getElementById('second');
+var third = document.getElementById('third');
+window.onscroll = function () { 
+    "use strict";
+    if (window.pageYOffset >= 200 ) {
+        nav.style.backgroundColor = '#A53860';
+        logo.style.color = '#fff';
+        first.style.color = '#fff';
+        second.style.color = '#fff';
+        third.style.color = '#fff';
+//        nav.classList.add("nav-colored");
+//        nav.classList.remove("nav-transparent");
+    } 
+    else {
+        nav.style.backgroundColor = '#fff';
+        logo.style.color = '#A53860';
+        first.style.color = '#000';
+        second.style.color = '#000';
+        third.style.color = '#000';
+//        nav.classList.add("nav-transparent");
+//        nav.classList.remove("nav-colored");
+    }
+};
